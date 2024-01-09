afac31400991585d821b3574421799120b790d90
carina-maleski <carinamaleski@gmail.com>
2024-01-09 15:41:19 -0300

    formatação

--- db_ecofamily.sql
+++ db_ecofamily.sql
@@ -5,27 +5,27 @@ use db_ecofamily;
 
 #criação tabelas
 create table tb_categorias (
-id BIGINT auto_increment primary key,
-descricao VARCHAR(255) not null,
+id bigint auto_increment primary key,
+descricao varchar(255) not null,
 perecivel boolean not null
 );
 
 create table tb_usuarios (
-id BIGINT auto_increment primary key,
-nome VARCHAR(255) not null,
-usuario VARCHAR(255) not null unique,
-senha VARCHAR(255) not null,
-foto VARCHAR(255)
+id bigint auto_increment primary key,
+nome varchar(255) not null,
+usuario varchar(255) not null unique,
+senha varchar(255) not null,
+foto varchar(255)
 );
 
 create table tb_produtos (
-id BIGINT auto_increment primary key,
-nome VARCHAR(255) not null unique,
-descricao VARCHAR(255),
-preco DECIMAL(10,2) not null,
-quantidade INT not null,
-categoria_id BIGINT,
-usuario_id BIGINT,
+id bigint auto_increment primary key,
+nome varchar(255) not null unique,
+descricao varchar(255),
+preco decimal(10,2) not null,
+quantidade int not null,
+categoria_id bigint,
+usuario_id bigint,
 foreign key (categoria_id) references tb_categorias(id),
 foreign key (usuario_id) references tb_usuarios(id)
 );
